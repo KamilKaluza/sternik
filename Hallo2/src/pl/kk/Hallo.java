d8aa7ac7529bca4feed2b7944ab3c5a57578269f
Kamil Kaluza <k.kaluza92@gmail.com>
2017-03-11 14:44:43 +0100

    reszta plikow

---
+++ Hallo2/src/pl/kk/Hallo.java
@@ -0,0 +1,10 @@
+package pl.kk;
+
+public class Hallo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hura ale szybko posz³o");  //syso +ctrl+spacja
+	}
+
+}
