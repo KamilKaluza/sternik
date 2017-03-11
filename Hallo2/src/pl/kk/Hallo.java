91d22d5fa72d9477cf75f1faa6ff1d93c702af41
Kamil Kaluza <k.kaluza92@gmail.com>
2017-03-11 15:31:11 +0100

    aaa

--- Hallo2/src/pl/kk/Hallo.java
+++ Hallo2/src/pl/kk/Hallo.java
@@ -4,7 +4,7 @@ public class Hallo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hura ale szybko posz³o");  //syso +ctrl+spacja
+		System.out.println("!!Hura ale szybko posz³o");  //syso +ctrl+spacja
 	}
 
 }
